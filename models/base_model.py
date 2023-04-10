719888097e9f6763e75af6ecc5f63a8bd258ad2f
Adedimeji <ojerindeadedimeji@gmail.com>
2023-04-10 15:15:26 -0700

    new file

---
+++ models/base_model.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+from daatetime import datetime
+"""
+
+class BaseModel that defines all common attributes/methods for other classes
+
+"""
+
+
+class BaseModel:
+    """
+
+    class BaseModel that defines all common attributes
+    /methods for other classes
+
+    """
+
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now().isoformat()
+        self.updated_at = datetime.now().isoformat()
+
+    def __str__(self):
+        return "[{}] ({}) {}"\
+                .format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.now().isoformat()
+
+    def to_dict(self):
+        self.__dict__["__class__"] = self.__class__.__name__
+        return self.__dict__
