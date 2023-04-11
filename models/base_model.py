09f23352f7c74858e023f7615219ef6a19fb8801
Adedimeji <ojerindeadedimeji@gmail.com>
2023-04-10 17:01:22 -0700

    changes

--- models/base_model.py
+++ models/base_model.py
@@ -16,10 +16,25 @@ class BaseModel:
 
     """
 
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "updated_at" or key == "created_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+            if "id" not in kwargs:
+                self.id = str(uuid.uuid4())
+            if "created_at" not in kwargs:
+                self.created_at = datetime.now()
+            if "created_at" in kwargs and "updated_at" not in kwargs:
+                self.updated_at = self.created_at
+            else:
+                self.updated_at = datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return "[{}] ({}) {}"\
