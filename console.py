c4ad699f8a0bc454ef79236f871e5de721dc8cf0
Adedimeji <ojerindeadedimeji@gmail.com>
2023-04-13 06:47:47 -0700

    first commit of file

---
+++ console.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import cmd
+"""
+
+a program called console.py that contains the entry point of the
+command interpreter
+
+"""
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        return True
+
+    def help_quit(self):
+        print("Quit command to exit the program")
+        print()
+
+    def do_EOF(self, arg):
+        return True
+
+    def help_EOF(self):
+        print("EOF command to exit the program")
+        print()
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
